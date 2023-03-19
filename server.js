8228b4b020c6072f11ffbc0259e4a85285b25e34
Glitch (glitch-hello-node) <none>
2023-03-19 21:26:33 +0000

    👴🌓 Checkpoint ./server.js:56073722/257

--- server.js
+++ server.js
@@ -7,18 +7,15 @@ const Port = 4000;
 const ws = new Websocket.Server({
   port : Port
 });
-
+let message;
 ws.on("connection", function (socket){
   console.log("A Client Entered")
   
-  socket.on("message",function(msg)
-           {
-    console.log("Message From a Client:" + msg)
-    ws.clients.forEach(client => {
-      client.send(msg);
+  socket.on("message",function(msg){
+    message = msg;
+    console.log("client sent this : " + message);
     });
   })
-})
 
 
 
