01444687f8a01c51690638f33e1bf8bfc2bb5bfd
Glitch (glitch-hello-node) <none>
2023-03-19 21:50:28 +0000

    🐀🍥 Checkpoint ./server.js:56073722/1362

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ ws.on("connection", function (socket){
   
   socket.on("message",function(msg){
     message = msg;
-    ws.clients.forEach(function each(client) {
-        client.send(msg);
+    if (msg = "blind"){ 
+      ws.clients.forEach(function each(client) {
+        client.send("/control.blind");
 
-    });
+    });}else
+    if (msg = "speach"){ 
+      ws.clients.forEach(function each(client) {
+        client.send("/control.speach");
 
+    });}else
+    if (msg = "context"){ 
+      ws.clients.forEach(function each(client) {
+        client.send("/control.context");
+
+    });}
     console.log("client sent this : " + message);
     
     });
