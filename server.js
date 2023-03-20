9b5f3e91a76438f98186e6f25b4264f34e24de08
Glitch (glitch-hello-node) <none>
2023-03-20 01:43:32 +0000

    🎷🍱 Checkpoint ./server.js:56073722/957

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ const ws = new Websocket.Server({
 let message;
 ws.on("connection", function (socket){
   console.log("A Client Entered")
-  
-  socket.on("message",function(msg){
-    message = msg;
-    console.log(msg);
-    ws.clients.forEach(function each(client) {
-      client.send("/" + msg)
-    });
-})})
+  socket.on('message', function message(data, isBinary) {
+  const message = isBinary ? data : data.toString();
+    console.log(message);
+    ws.clients.forEach(function (client){
+      client.send("/" + message)
+    })
+});
+
+})
 
