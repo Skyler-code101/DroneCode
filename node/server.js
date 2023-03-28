f97671f64584e2dc2405b37aa38106f6a3451f14
Glitch (glitch-hello-node) <none>
2023-03-28 00:10:21 +0000

    ☺️📌 Checkpoint ./node/server.js:56073722/528 ./node/file.js:56073722/12 ./package.json:56073722/1071 ./index.html:56073722/483

--- node/server.js
+++ node/server.js
@@ -1,11 +1,22 @@
+var http = require('http'),
+    fs = require('fs');
 
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
 // Websocket ip === wss://daffy-nervous-chatter.glitch.me/
 
-const secondnode = require('child_process').fork('file.js')
 const Websocket = require('ws');
 
 const Port = 4000;
-
 
 const ws = new Websocket.Server({
   port : Port
