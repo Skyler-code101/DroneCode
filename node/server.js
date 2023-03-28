84a2f547599b7c8ea78ebcb0d48fede573ec6377
Glitch (glitch-hello-node) <none>
2023-03-28 00:58:20 +0000

    🏍🐊 Checkpoint ./node/server.js:56073722/1954

--- node/server.js
+++ node/server.js
@@ -1,7 +1,22 @@
 
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+
 const Websocket = require('ws');
 
-const Port = 4000;
+const Port = 9000;
 
 const ws = new Websocket.Server({
   port : Port
@@ -17,4 +32,3 @@ ws.on("connection", function (socket){
 });
 
 })
-
